72fa4223b1f6c2ba488ec0a4da60e1eaf51b7c83
Wesnei <wesneipaiva@gmail.com>
2025-08-26 11:17:18 -0300

    feat: implementation JS

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+// Definição de variáveis e busca de ID
+const input = document.getElementById("tarefaInput");
+const botao = document.getElementById("adicionarBtn");
+const lista = document.getElementById("listaTarefas");
+
+// Criando função
+function adicionarTarefa() {
+  const texto = input.value.trim();
+  
+  if (texto === "") {
+    alert("Digite uma tarefa!");
+    return;
+  }
+
+  //Adicionando item na lista
+  const item = document.createElement("li");
+  item.innerText = texto;
+
+  item.addEventListener("click", function() {
+    item.classList.toggle("done");
+  });
+
+  const btnRemover = document.createElement("button");
+  btnRemover.innerText = "X";
+  btnRemover.addEventListener("click", function(e) {
+    e.stopPropagation(); 
+    lista.removeChild(item);
+  });
+
+  item.appendChild(btnRemover);
+  lista.appendChild(item);
+
+  input.value = "";
+}
+
+botao.addEventListener("click", adicionarTarefa);
